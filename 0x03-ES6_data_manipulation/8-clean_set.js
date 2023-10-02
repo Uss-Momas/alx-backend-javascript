b86015254716e22bf75beea08033736cd691a71c
Ussumane Momade <89344441+Uss-Momas@users.noreply.github.com>
2023-10-02 11:36:49 +0200

    function that creates a new Map type object

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,9 @@
+export default function cleanSet(set, startString) {
+    const arrayFromSet  = [...set];
+    for (const element of arrayFromSet) {
+        for (const [index, letter] of startString) {
+            
+        }
+    }
+    return [...set].join("-");
+}
