225ccc27c512be22ea83647bc75f7c279982f021
Ussumane Momade <89344441+Uss-Momas@users.noreply.github.com>
2023-10-04 16:36:03 +0200

    fix: add condition if startString is empty return empty string

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,9 @@
 export default function cleanSet(set, startString) {
   const arrayFromSet = [...set];
   const newArray = [];
+
+  if (startString === '') return newArray.join('-');
+
   for (const element of arrayFromSet) {
     if (element.startsWith(startString)) { newArray.push(element.replace(startString, '')); }
   }
