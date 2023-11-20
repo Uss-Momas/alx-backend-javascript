91131c7f7205f14d2d0292745ebdaf099e66bb58
Ussumane Momade <89344441+Uss-Momas@users.noreply.github.com>
2023-11-20 12:26:39 +0200

    Create server with express and gather user information

---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const countStudents = require('./3-read_file_async');
+
+const app = express();
+
+const port = 1245;
+
+app.get('/', (req, res) => {
+    res.send('Hello Holberton School!');
+});
+
+app.get('/students', (req, response) => {
+    console.log(process.argv[2]);
+    countStudents(process.argv[2])
+        .then((result) => {
+            let message = 'This is the list of our students\n';
+            const field_stats = result.field_stats;
+            const totalStudents = result.numberStudents;
+            message += `Number of students: ${totalStudents}\n`;
+
+            for(const [field, object] of Object.entries(field_stats)) {
+                message += `Number of students in ${field}: ${object.total}. List: ${object.listStudens.join(", ")}\n`;
+            }
+            response.send(message);
+        })
+        .catch((err) => {
+            response.status(404);
+            response.send(err.message);
+        });
+});
+
+app.listen(port, () => {
+    // console.log(`Example app listening on port ${port}`);
+})
+
+module.exports = app;
