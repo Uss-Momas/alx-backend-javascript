4f139104f78beb84b98338fe6395931f07594286
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-11-21 16:46:32 +0200

    use countStudents from within the file

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,6 +1,28 @@
 const http = require('http');
+const fs = require('fs');
 
-const countStudents = require('./3-read_file_async');
+async function countStudents(path) {
+  try {
+    const data = await fs.promises.readFile(path, 'utf8');
+    const contents = data.toString().trim().split('\n').splice(1)
+      .map((student) => student.trim());
+    const studentsObject = contents.map((student) => student.split(',')).map((arrayStudent) => ({ firstName: arrayStudent[0], field: arrayStudent[3] }));
+    const studentsByField = {};
+    const fields = [...new Set(studentsObject.map((studentObj) => studentObj.field))];
+
+    for (const field of fields) {
+      studentsByField[field] = [];
+    }
+
+    for (const student of studentsObject) {
+      studentsByField[student.field].push(student.firstName);
+    }
+
+    return { numStudents: contents.length, studentsByField };
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+}
 
 const hostname = '127.0.0.1';
 const port = 1245;
@@ -15,7 +37,6 @@ const app = http.createServer((req, res) => {
   }
   if (url === '/students') {
     const result = countStudents(process.argv[2]);
-
     result.then((value) => {
       const { studentsByField } = value;
       res.write('This is the list of our students\n');
