d8059d28eabd97d615c21a56d1a9970516d9bc4f
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-11-21 15:42:44 +0200

    fix linter error

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,50 +1,12 @@
-const fs = require('fs/promises');
+const fs = require('fs');
 
 async function countStudents(path) {
-  let contents;
   try {
-    contents = await fs.readFile(path, 'utf-8');
+    const contents = await fs.promises.readFile(path, 'utf8');
+    console.log(contents);
   } catch (error) {
     throw new Error('Cannot load the database');
   }
-
-  // transform as array and remove the first line (header)
-  contents = contents.toString().trim().split('\n').splice(1);
-
-  // array containing the fields names
-  let fields = contents.map((line) => {
-    const newLine = line.split(',');
-    const field = newLine[newLine.length - 1];
-    return field.trim();
-  });
-
-  // object for the stats
-  const fieldStats = {};
-
-  // remove duplicates
-  fields = [...new Set(fields)];
-
-  // initializing the object with the stats for each field
-  for (const field of fields) {
-    fieldStats[field] = { total: 0, listStudens: [] };
-  }
-
-  // updating stats for each field
-  for (let line of contents) {
-    line = line.split(',');
-    const field = line[line.length - 1].trim();
-    const firstname = line[0].trim();
-    fieldStats[field].total += 1;
-    fieldStats[field].listStudens.push(firstname);
-  }
-  // Stats
-  // total number of students
-  console.log(`Number of students: ${contents.length}`);
-  for (const [field, object] of Object.entries(fieldStats)) {
-    // print total number of students per field, and firstname of each
-    console.log(`Number of students in ${field}: ${object.total}. List: ${object.listStudens.join(', ')}`);
-  }
-  return { numberStudents: contents.length, fieldStats };
 }
 
 module.exports = countStudents;
