2a9afc0098ec050ddfafe0064a8a625b8be8f336
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-11-21 10:41:28 +0200

    Fix eslint

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,10 +3,10 @@ process.stdin.setEncoding('utf8');
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('readable', () => {
-    const name_input = process.stdin.read();
-    if (name_input !== null) {
-      process.stdout.write(`Your name is: ${name_input}`);
-    }
-  });
+  const name = process.stdin.read();
+  if (name !== null) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
+});
 
 process.stdin.on('end', () => { process.stdout.write('This important software is now closing\n'); });
