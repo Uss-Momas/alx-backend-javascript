52339325ee03c827c18f752a9116a403d9317ff2
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-11-21 16:19:17 +0200

    Refactor: group students by field

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -2,8 +2,25 @@ const fs = require('fs');
 
 async function countStudents(path) {
   try {
-    const contents = await fs.promises.readFile(path, 'utf8');
-    console.log(contents);
+    const data = await fs.promises.readFile(path, 'utf8');
+    const contents = data.toString().trim().split('\n').splice(1)
+      .map((student) => student.trim());
+    const studentsObject = contents.map((student) => student.split(',')).map((arrayStudent) => ({ firstName: arrayStudent[0], field: arrayStudent[3] }));
+    const studentsByField = {};
+    const fields = [...new Set(studentsObject.map((studentObj) => studentObj.field))];
+
+    for (const field of fields) {
+      studentsByField[field] = [];
+    }
+
+    for (const student of studentsObject) {
+      studentsByField[student.field].push(student.firstName);
+    }
+
+    console.log(`Number of students: ${contents.length}`);
+    for (const [field, list] of Object.entries(studentsByField)) {
+      console.log(`Number of students in ${field}: ${list.length}. List: ${list.join(', ')}`);
+    }
   } catch (error) {
     throw new Error('Cannot load the database');
   }
