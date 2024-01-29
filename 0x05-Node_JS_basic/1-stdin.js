83e55a7851fd35bd423b491012e784cb3df10e30
Ussumane Momade <89344441+Uss-Momas@users.noreply.github.com>
2024-01-29 10:12:11 +0200

    Fix eslint error

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,14 +2,14 @@ process.stdin.setEncoding('utf8');
 
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('readable', function () {
-    const chunk = process.stdin.read();
-    if (chunk !== null) {
-        process.stdout.write('Your name is: ' + chunk);
-    }
-    process.stdout.write('This important software is now closing');
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
+  if (chunk !== null) {
+    process.stdout.write(`Your name is: ${chunk}`);
+  }
+  process.stdout.write('This important software is now closing');
 });
 
-process.stdin.on('end', function () {
-    process.stdout.write('This important software is now closing');
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing');
 });
