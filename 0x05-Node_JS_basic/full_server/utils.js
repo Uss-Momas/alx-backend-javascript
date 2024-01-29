3e012988659afebaca817a8171a0a56d359c8f75
ussumane <ussumane.momade@uem.ac.mz>
2024-01-29 23:26:14 +0200

    create function to read database in asynchronous mode

--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -1,3 +1,43 @@
-async function readDatabase(path) {
-    
+import fs from 'fs';
+
+function processStudents(studentsData) {
+    const students = studentsData.split('\n').map((student) => {
+        const trimedStudent = student.trim().split(',');
+        return [trimedStudent[0], trimedStudent[3]];
+
+    })
+    return students.splice(1);
 }
+
+function infoPerField(students) {
+    const objectOfFields = {};
+    for (const [firstname, field] of students) {
+        objectOfFields[field] = { total: 0, list: [] };
+    }
+
+    for (const [firstname, field] of students) {
+        objectOfFields[field].total += 1;
+        objectOfFields[field].list.push(firstname);
+    }
+
+    return objectOfFields;
+}
+
+async function readDatabase(path) {
+    if (!fs.existsSync(path)) {
+        throw new Error('Cannot load the database');
+    }
+
+    if (!fs.lstatSync(path).isFile()) {
+        throw new Error('Cannot load the database');
+    }
+
+    const students = await fs.promises.readFile(path, { encoding: 'utf-8' });
+    const newStudents = processStudents(students);
+
+    const fieldInfo = infoPerField(newStudents);
+
+    return fieldInfo;
+}
+
+export default readDatabase;
