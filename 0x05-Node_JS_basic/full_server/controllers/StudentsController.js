fe26e4315f899756f570615aba52e01569f81a79
ussumane <ussumane.momade@uem.ac.mz>
2024-01-29 23:27:32 +0200

    implement get all students and get students by major features

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -1,5 +1,34 @@
-class StudentsController{
-    static getAllStudents(request, response) {
-        return response.send('getAllStudents function');
+import readDatabase from "../utils";
+const PATH = process.argv[2];
+
+export default class StudentsController {
+    static async getAllStudents(request, response) {
+        try {
+            const fieldInfo = await readDatabase(PATH);
+
+            const messages = ['This is the list of our students'];
+
+            for (const [field, object] of Object.entries(fieldInfo)) {
+                messages.push(`Number of students in ${field}: ${object.total}. List: ${object.list.join(', ')}`)
+            }
+            return response.send(messages.join('\n'));
+        } catch (error) {
+            return response.status(500).send(error.message);
+        }
+    }
+
+    static async getAllStudentsByMajor(request, response) {
+        const { major } = request.params;
+
+        if (major !== 'CS' && major !== 'SWE') {
+            return response.status(500).send('Major parameter must be CS or SWE');
+        }
+        try {
+            const fieldInfo = await readDatabase(PATH);
+
+            return response.send(`List: ${fieldInfo[major].list.join(', ')}`);
+        } catch (error) {
+            return response.status(500).send(error.message);
+        }
     }
 }
