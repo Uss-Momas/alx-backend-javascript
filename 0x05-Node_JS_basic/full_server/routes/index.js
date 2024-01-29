4cfa2a2f70c7964b364595d8e5b265d7743cb8de
ussumane <ussumane.momade@uem.ac.mz>
2024-01-29 23:25:43 +0200

    create routes with express Router

---
+++ 0x05-Node_JS_basic/full_server/routes/index.js
@@ -0,0 +1,11 @@
+import AppController from '../controllers/AppController';
+import StudentsController from '../controllers/StudentsController';
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/', (req, res) => AppController.getHomePage(req, res));
+router.get('/students', async (req, res) => StudentsController.getAllStudents(req, res));
+router.get('/students/:major', async (req, res) => StudentsController.getAllStudentsByMajor(req, res));
+
+export default router;
