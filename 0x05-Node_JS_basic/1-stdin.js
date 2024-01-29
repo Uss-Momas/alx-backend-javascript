caf842b8aaa016a0f8a6afa6ef8f456463883e37
Ussumane Momade <89344441+Uss-Momas@users.noreply.github.com>
2024-01-29 10:10:46 +0200

    Using Process stdin

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,15 @@
+process.stdin.setEncoding('utf8');
+
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('readable', function () {
+    const chunk = process.stdin.read();
+    if (chunk !== null) {
+        process.stdout.write('Your name is: ' + chunk);
+    }
+    process.stdout.write('This important software is now closing');
+});
+
+process.stdin.on('end', function () {
+    process.stdout.write('This important software is now closing');
+});
