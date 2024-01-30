e739aa538247d6bd2872fb2fcc21c1e0e350713f
ussumane <ussumane.momade@uem.ac.mz>
2024-01-30 18:03:42 +0200

    comment code for test purposes

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -9,29 +9,32 @@ function countStudents(path) {
     throw new Error('Cannot load the database');
   }
 
-  const students = fs.readFileSync(path, { encoding: 'utf8' }).split('\n').map((student) => student.trim()).splice(1);
+  // const students = fs.readFileSync(path, { encoding: 'utf8' })
+  // .split('\n').map((student) => student.trim()).splice(1);
 
-  const NUMBER_OF_STUDENTS = students.length;
+  // const NUMBER_OF_STUDENTS = students.length;
 
-  console.log(`Number of students: ${NUMBER_OF_STUDENTS}`);
+  // console.log(`Number of students: ${NUMBER_OF_STUDENTS}`);
 
-  const fieldOfStudents = {};
-  for (const student of students) {
-    const studentInfo = student.split(',');
-    const field = studentInfo[studentInfo.length - 1];
-    fieldOfStudents[field] = { total: 0, listStudens: [] };
-  }
+  // const fieldOfStudents = {};
+  // for (const student of students) {
+  //   const studentInfo = student.split(',');
+  //   const field = studentInfo[studentInfo.length - 1];
+  //   fieldOfStudents[field] = { total: 0, listStudens: [] };
+  // }
 
-  for (const student of students) {
-    const studentInfo = student.split(',');
-    const field = studentInfo[studentInfo.length - 1];
-    fieldOfStudents[field].total += 1;
-    fieldOfStudents[field].listStudens.push(studentInfo[0]);
-  }
+  // for (const student of students) {
+  //   const studentInfo = student.split(',');
+  //   const field = studentInfo[studentInfo.length - 1];
+  //   fieldOfStudents[field].total += 1;
+  //   fieldOfStudents[field].listStudens.push(studentInfo[0]);
+  // }
 
-  for (const [FIELD, object] of Object.entries(fieldOfStudents)) {
-    console.log(`Number of students in ${FIELD}: ${object.total}. List: ${object.listStudens.join(', ')}`);
-  }
+  // for (const [FIELD, object] of Object.entries(fieldOfStudents)) {
+  //   console.log(
+  //     `Number of students in ${FIELD}: ${object.total}. List: ${object.listStudens.join(', ')}`
+  //   );
+  // }
 }
 
 module.exports = countStudents;
