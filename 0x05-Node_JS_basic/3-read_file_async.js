6d43899144d7015f8f277158b5a66495dc5cd49e
ussumane <ussumane.momade@uem.ac.mz>
2024-01-30 18:49:42 +0200

    refactor: process students information and log the messages

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -8,6 +8,38 @@ async function countStudents(path) {
   if (!fs.statSync(path).isFile()) {
     throw new Error('Cannot load the database');
   }
+
+  try {
+    const students = (await fs.promises.readFile(path, { encoding: 'utf-8' })).toString().trim().split('\n').splice(1)
+      .map((student) => student.trim());
+
+    const NUMBER_OF_STUDENTS = students.length;
+    const messages = [`Number of students: ${NUMBER_OF_STUDENTS}`];
+
+    const processedStudents = students.map((student) => {
+      const studentInfo = student.split(',');
+      return { firstname: studentInfo[0], field: studentInfo[3] };
+    });
+
+    const fieldInfo = {};
+
+    for (const student of processedStudents) {
+      fieldInfo[student.field] = { total: 0, list: [] };
+    }
+
+    for (const student of processedStudents) {
+      fieldInfo[student.field].total += 1;
+      fieldInfo[student.field].list.push(student.firstname);
+    }
+
+    for (const [field, statsObj] of Object.entries(fieldInfo)) {
+      messages.push(`Number of students in ${field}: ${statsObj.total}. List: ${statsObj.list.join(', ')}`);
+    }
+
+    console.log(messages.join('\n'));
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
 }
 
 module.exports = countStudents;
